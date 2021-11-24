b2c7acf4dc66b56d8ad96feef6d5726765b1fd6d
Rod Morgado <rodrigo@zooft.company>
2021-11-23 19:07:39 -0800

    finding values out of range and replacing them with low and max limits

--- glimpse.R
+++ glimpse.R
@@ -25,3 +25,32 @@ assert_is_factor(bike_share_rides$user_birth_year_fct)
 
 # Summary of user_birth_year_fct
 summary(bike_share_rides$user_birth_year_fct)
+
+bike_share_rides <- bike_share_rides %>%
+  # Remove 'minutes' from duration: duration_trimmed
+  mutate(duration_trimmed = str_remove(duration, "minutes"),
+        duration_mins = as.numeric(duration_trimmed))
+         # Convert duration_trimmed to numeric: duration_mins
+
+# Glimpse at bike_share_rides
+glimpse(bike_share_rides)
+
+# Assert duration_mins is numeric
+assert_is_numeric(bike_share_rides$duration_mins)
+
+# Calculate mean duration
+mean(bike_share_rides$duration_mins)
+
+# Create breaks
+breaks <- c(min(bike_share_rides$duration_min), 0, 1440, max(bike_share_rides$duration_min))
+
+# Create a histogram of duration_min
+ggplot(bike_share_rides, aes(duration_min)) +
+  geom_histogram(breaks = breaks)
+
+# duration_min_const: replace vals of duration_min > 1440 with 1440
+bike_share_rides <- bike_share_rides %>%
+  mutate(duration_min_const = replace(duration_min, duration_min > 1440, 1440))
+
+# Make sure all values of duration_min_const are between 0 and 1440
+assert_all_are_in_closed_range(bike_share_rides$duration_min_const, lower = 0, upper = 1440)
