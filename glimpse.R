4a0892b0cf2ff59066d3d20cf79e083017d26a09
Rod Morgado <rodrigo@zooft.company>
2021-11-23 18:44:39 -0800

    working with glimpse to summarize the data and assert_is_a_factor

---
+++ glimpse.R
@@ -0,0 +1,27 @@
+# This is the first file to learn how to clean the data
+
+# Glimpse at bike_share_rides
+glimpse(bike_share_rides)
+
+# Summary of user_birth_year
+summary(bike_share_rides$user_birth_year)# Glimpse at bike_share_rides
+glimpse(bike_share_rides)
+
+# Summary of user_birth_year
+summary(bike_share_rides$user_birth_year)
+
+# Glimpse at bike_share_rides
+glimpse(bike_share_rides)
+
+# Summary of user_birth_year
+summary(bike_share_rides$user_birth_year)
+
+# Convert user_birth_year to factor: user_birth_year_fct
+bike_share_rides <- bike_share_rides %>%
+  mutate(user_birth_year_fct = as.factor(bike_share_rides$user_birth_year))
+
+# Assert user_birth_year_fct is a factor
+assert_is_factor(bike_share_rides$user_birth_year_fct)
+
+# Summary of user_birth_year_fct
+summary(bike_share_rides$user_birth_year_fct)
