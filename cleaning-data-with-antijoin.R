fd21d4969a61d7c3315fbd29b10df740d2e3b409
Rod Morgado <rodrigo@zooft.company>
2021-11-24 18:21:19 -0800

    using semi_join and anti_join to get corrupted values

---
+++ cleaning-data-with-antijoin.R
@@ -0,0 +1,13 @@
+# Find bad dest_size rows
+sfo_survey %>% 
+  # Join with dest_sizes data frame to get bad dest_size rows
+  anti_join(dest_sizes, by = "dest_size")  %>%
+  # Select id, airline, destination, and dest_size cols
+  select(id, airline, destination, dest_size)
+
+  # Remove bad dest_size rows
+sfo_survey %>% 
+  # Join with dest_sizes
+  semi_join(dest_sizes, by = "dest_size") %>%
+  # Count the number of each dest_size
+  count(dest_size)
