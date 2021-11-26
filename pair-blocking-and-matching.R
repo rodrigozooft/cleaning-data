126d97c286819af01121e19a349a15d0e49b24a3
Rod Morgado <rodrigo@zooft.company>
2021-11-26 13:33:47 -0800

    using compare pairs and pair_blocking to find similar strings in different tables

---
+++ pair-blocking-and-matching.R
@@ -0,0 +1,17 @@
+# Load reclin
+library(reclin)
+
+# Generate pairs with same city
+pair_blocking(zagat, fodors, blocking_var = "city")
+
+# Generate pairs
+pair_blocking(zagat, fodors, blocking_var = "city") %>%
+  # Compare pairs by name using lcs()
+  compare_pairs(by = "name",
+      default_comparator = lcs())
+
+# Generate pairs
+pair_blocking(zagat, fodors, blocking_var = "city") %>%
+  # Compare pairs by name, phone, addr
+  compare_pairs(by = c("name", "phone", "addr"),
+                default_comparator = jaro_winkler())
