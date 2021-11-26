5fe1f9554f7d1894d834067d051b6ce2e0fa173e
Rod Morgado <rodrigo@zooft.company>
2021-11-26 13:40:56 -0800

    pair blocking using a blocking var, comparing_pairs, scoring, selecting and linking

--- pair-blocking-and-matching.R
+++ pair-blocking-and-matching.R
@@ -15,3 +15,14 @@ pair_blocking(zagat, fodors, blocking_var = "city") %>%
   # Compare pairs by name, phone, addr
   compare_pairs(by = c("name", "phone", "addr"),
                 default_comparator = jaro_winkler())
+
+# Create pairs
+pair_blocking(zagat, fodors, blocking_var = "city") %>%
+  # Compare pairs
+  compare_pairs(by = "name", default_comparator = jaro_winkler()) %>%
+  # Score pairs
+  score_problink() %>%
+  # Select pairs
+  select_n_to_m() %>%
+  # Link data 
+  link()
